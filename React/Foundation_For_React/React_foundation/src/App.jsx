d4c30b57ced4c29086fca0b3af1d69c76ad62453
RevanthMali <malirevanth06@gmail.com>
2024-02-18 11:42:54 +0530

    modified foundation_for_react files

--- React/Foundation_For_React/React_foundation/src/App.jsx
+++ React/Foundation_For_React/React_foundation/src/App.jsx
@@ -3,17 +3,35 @@ import { useState } from 'react'
 // import viteLogo from '/vite.svg'
 import './App.css'
 
+let state ={
+  count: 0
+}
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count,setCount] = useState(0);
+  // function onClickHandler(){
+  //   setCount(count+1);
+  //   // state.count = state.count +1; this is not the right way to update a state variable instead user a fucntion 'setCount'
+  // }
   return (
-     
-      <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-  )
+    <div>
+    {/* <button onClick={onClickHandler}>Counter:{count}</button> */}
+       {/* custom components creation */}
+      <CustomButton count={count} setCount = {setCount}></CustomButton>
+      <CustomButton count={count+1} setCount = {setCount}></CustomButton>
+      <CustomButton count={count-1} setCount = {setCount}></CustomButton>
+      <CustomButton count={count*2} setCount = {setCount}></CustomButton>
+    </div>
+  ) 
+  // component- with this we can create various custom action which update dynamically
+  function CustomButton(props){
+    function onClickHandler(){
+      props.setCount(props.count+1);
+    }
+    return <button onClick={onClickHandler}>Counter: {props.count}</button>
+  }
+
+  
 }
 
 export default App
+ 
