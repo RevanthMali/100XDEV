e391ea9ebf9030d3d5f6685589a5200314156e6b
RevanthMali <malirevanth06@gmail.com>
2024-03-04 18:37:03 +0530

    recoil and state management

--- React/routes/src/App.jsx
+++ React/routes/src/App.jsx
@@ -3,10 +3,11 @@
 // const Dashboard = lazy(() => import('./components/Dashboard'));
 // const Landing = lazy(() => import('./components/Landing'));
 // import { useContext, useState } from 'react';
+import { useMemo } from 'react';
 import './App.css';
 import { CountContext } from './context';
-import { countAtom } from './store/atoms/count';
-import {RecoilRoot,useRecoilState,useRecoilValue} from "recoil"
+import { countAtom, evenSelector } from './store/atoms/count';
+import {RecoilRoot,useRecoilValue,useSetRecoilState} from "recoil"
 // function App() {
 //   return (
 //     <div>
@@ -97,25 +98,40 @@ function Count({count,setCount}){
     return <div>
       <CountRenderer/>
       <Buttons/>
+      <EvenCountRenderer/>
     </div>
    } 
    function CountRenderer(){
+    // To actually get the value from the atom we use UseRecoilValue hook
      const count = useRecoilValue(countAtom);
 
      return <div>
       {count}
     </div>
    }
+   function EvenCountRenderer(){
+      const count = useRecoilValue(countAtom);
+      // this logic(useMemo) will run only if the value of the count changes
+      // const isEven = useMemo(()=>{
+      //   return count%2==0;
+      // },[count]); 
+      // evenselector is declared in count.jsx as a selector 
+      const isEven = useRecoilValue(evenSelector);
+      return <div>
+        {isEven ? "Its even value":null}
+      </div>
+   }
    function Buttons(){ 
-     const [count,setCount] = useRecoilState(countAtom);
-
+    // usesetrecoilState re-renders only the componenet which keeps changing, reduces unnecessary er-renders
+     const setCount= useSetRecoilState(countAtom);
+    console.log("re-rendered")
      return <div>  
       <button onClick={()=>{
-        setCount(count+1);
+        setCount(count => count+1);
       }}>Increase</button>
 
       <button onClick={()=>{
-        setCount(count-1);
+        setCount(count => count-1);
       }}>Decrease</button>
     </div>
   }
